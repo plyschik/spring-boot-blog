f6685bb833c56a3ae4af9a87045b7bf40f4f2cf8
plyschik <palyschik@gmail.com>
2021-06-30 10:03:52 +0200

    Add tag edit form on dashboard.

--- src/main/java/io/plyschik/springbootblog/controller/dashboard/TagController.java
+++ src/main/java/io/plyschik/springbootblog/controller/dashboard/TagController.java
@@ -3,16 +3,14 @@ package io.plyschik.springbootblog.controller.dashboard;
 import io.plyschik.springbootblog.dto.Alert;
 import io.plyschik.springbootblog.dto.TagDto;
 import io.plyschik.springbootblog.exception.TagAlreadyExists;
+import io.plyschik.springbootblog.exception.TagNotFound;
 import io.plyschik.springbootblog.service.TagService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -64,5 +62,51 @@ public class TagController {
         );
 
         return new ModelAndView("redirect:/dashboard/tags");
+    }
+
+    @GetMapping("/dashboard/tags/{id}/edit")
+    public ModelAndView showEditForm(@PathVariable long id, RedirectAttributes redirectAttributes) {
+        try {
+            ModelAndView modelAndView = new ModelAndView("dashboard/tag/edit");
+            modelAndView.addObject("id", id);
+            modelAndView.addObject("tag", tagService.getTagForEdit(id));
+
+            return modelAndView;
+        } catch (TagNotFound exception) {
+            redirectAttributes.addFlashAttribute("alert", new Alert("danger", exception.getMessage()));
+
+            return new ModelAndView("redirect:/dashboard/tags");
+        }
+    }
+
+    @PostMapping("/dashboard/tags/{id}/edit")
+    public ModelAndView processEditForm(
+        @PathVariable long id,
+        @ModelAttribute("tag") @Valid TagDto tagDto,
+        BindingResult bindingResult,
+        RedirectAttributes redirectAttributes
+    ) {
+        try {
+            if (bindingResult.hasErrors()) {
+                return new ModelAndView("dashboard/tag/edit");
+            }
+
+            tagService.updateTag(id, tagDto);
+
+            redirectAttributes.addFlashAttribute(
+                "alert",
+                new Alert("success", "Tag has been successfully updated.")
+            );
+
+            return new ModelAndView("redirect:/dashboard/tags");
+        } catch (TagNotFound exception) {
+            redirectAttributes.addFlashAttribute("alert", new Alert("danger", exception.getMessage()));
+
+            return new ModelAndView("redirect:/dashboard/tags");
+        } catch (TagAlreadyExists exception) {
+            bindingResult.rejectValue("name", "error.name", exception.getMessage());
+
+            return new ModelAndView("dashboard/tag/edit");
+        }
     }
 }
--- src/main/java/io/plyschik/springbootblog/service/TagService.java
+++ src/main/java/io/plyschik/springbootblog/service/TagService.java
@@ -3,6 +3,7 @@ package io.plyschik.springbootblog.service;
 import io.plyschik.springbootblog.dto.TagDto;
 import io.plyschik.springbootblog.entity.Tag;
 import io.plyschik.springbootblog.exception.TagAlreadyExists;
+import io.plyschik.springbootblog.exception.TagNotFound;
 import io.plyschik.springbootblog.repository.TagRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -24,6 +25,26 @@ public class TagService {
         }
 
         Tag tag = new Tag();
+        tag.setName(tagDto.getName());
+
+        tagRepository.save(tag);
+    }
+
+    public TagDto getTagForEdit(long id) throws TagNotFound {
+        Tag tag = tagRepository.findById(id).orElseThrow(TagNotFound::new);
+
+        TagDto tagDto = new TagDto();
+        tagDto.setName(tag.getName());
+
+        return tagDto;
+    }
+
+    public void updateTag(long id, TagDto tagDto) throws TagNotFound, TagAlreadyExists {
+        if (tagRepository.existsByName(tagDto.getName())) {
+            throw new TagAlreadyExists();
+        }
+
+        Tag tag = tagRepository.findById(id).orElseThrow(TagNotFound::new);
         tag.setName(tagDto.getName());
 
         tagRepository.save(tag);
