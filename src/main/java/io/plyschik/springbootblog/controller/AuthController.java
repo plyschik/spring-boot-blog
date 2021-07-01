ecd57951ac90f7adee18aa910d1c9ecd657e71c5
plyschik <palyschik@gmail.com>
2021-07-01 10:24:01 +0200

    Translations improvements.

--- src/main/java/io/plyschik/springbootblog/controller/AuthController.java
+++ src/main/java/io/plyschik/springbootblog/controller/AuthController.java
@@ -4,6 +4,9 @@ import io.plyschik.springbootblog.dto.UserDto;
 import io.plyschik.springbootblog.entity.Role;
 import io.plyschik.springbootblog.exception.EmailAddressIsAlreadyTaken;
 import io.plyschik.springbootblog.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -15,12 +18,10 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import javax.validation.Valid;
 
 @Controller
+@RequiredArgsConstructor
 public class AuthController {
     private final UserService userService;
-
-    public AuthController(UserService userService) {
-        this.userService = userService;
-    }
+    private final MessageSource messageSource;
 
     @GetMapping("/auth/signup")
     public String signUpForm(Model model) {
@@ -42,14 +43,26 @@ public class AuthController {
         try {
             userService.signUp(userDto, Role.USER);
         } catch (EmailAddressIsAlreadyTaken exception) {
-            bindingResult.rejectValue("email", "error.email", exception.getMessage());
+            bindingResult.rejectValue(
+                "email",
+                "error.email",
+                messageSource.getMessage(
+                    "message.email_is_already_taken",
+                    null,
+                    LocaleContextHolder.getLocale()
+                )
+            );
 
             return "auth/signup";
         }
 
         redirectAttributes.addFlashAttribute(
             "message",
-            "User account has been successfully created! Now you can sign in."
+            messageSource.getMessage(
+                "message.account_has_been_successfully_created",
+                null,
+                LocaleContextHolder.getLocale()
+            )
         );
 
         return "redirect:/auth/signin";
