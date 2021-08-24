02bbac86944d33901025eaf3024ae9366afb1b5c
plyschik <palyschik@gmail.com>
2021-08-24 18:33:30 +0200

    Add sort and search to tags list.

--- src/main/java/io/plyschik/springbootblog/repository/TagRepository.java
+++ src/main/java/io/plyschik/springbootblog/repository/TagRepository.java
@@ -1,9 +1,12 @@
 package io.plyschik.springbootblog.repository;
 
+import io.plyschik.springbootblog.dto.TagWithPostsCount;
 import io.plyschik.springbootblog.entity.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +18,11 @@ public interface TagRepository extends JpaRepository<Tag, Long> {
     Page<Tag> findAllByOrderByIdDesc(Pageable pageable);
 
     boolean existsByName(String name);
+
+    @Query("SELECT new io.plyschik.springbootblog.dto.TagWithPostsCount(t.id, t.name, COUNT(p.id) AS postsCount) " +
+           "FROM Tag t " +
+           "LEFT JOIN t.posts p " +
+           "WHERE t.name LIKE %:name% " +
+           "GROUP BY t.id")
+    Page<TagWithPostsCount> findAllWithPostsCountWhereNameLike(@Param("name") String name, Pageable pageable);
 }
