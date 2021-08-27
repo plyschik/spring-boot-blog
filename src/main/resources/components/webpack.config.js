f808fd46077da5e827f5117bd57879d7550d4a1a
plyschik <palyschik@gmail.com>
2021-08-27 17:20:53 +0200

    Introduce React.

---
+++ src/main/resources/components/webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    comments: './src/comments',
+  },
+  output: {
+    path: path.resolve(__dirname, '../static/assets/js'),
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              '@babel/preset-env',
+              '@babel/preset-react',
+            ]
+          }
+        }
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  }
+};
